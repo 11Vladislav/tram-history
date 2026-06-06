c17a1baac51a7605302fa4254fd45f816c1435e7
Vladislav <vlad11ponomarev@gmail.com>
2026-06-06 21:28:58 +0300

    upd routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import GlobalStyles from "./GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <BrowserRouter  > */}
+        <HashRouter >
             <GlobalStyles />
             <App />
-        {/* </BrowserRouter> */}
+        </HashRouter>
     </React.StrictMode>
 );
